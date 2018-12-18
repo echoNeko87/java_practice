9f8db034b43e619ac1b591a430db7910b70b5783
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2018-12-18 14:01:35 -0500

    Initial Commit. Basic conversation in Esperanto.

---
+++ myJavaLibrary/src/main/java/com/ladylynn/myjavalibrary/MyClass.java
@@ -0,0 +1,14 @@
+package com.ladylynn.myjavalibrary;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Saluton Tifanja! Kiel vi fartas?");
+        System.out.println("Mi fartas bone. Dankon. Kaj vi?");
+        System.out.println("Bone, dankon. Pardonu, mi estas komencanto.");
+        System.out.println("Ĝi estas bone. Ĝis la revido, Tifanja!");
+
+    }
+
+}
